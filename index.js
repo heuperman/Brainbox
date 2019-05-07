38bd7c4c371e432d2040679868b6ea24c0ad1df6
Cornelis Heuperman <cheuperman@gmail.com>
2019-05-07 19:48:53 +0100

    Add service worker and offline page

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const textArea = document.getElementById('note-text');
-textArea.addEventListener('keyup', save);
-window.onload = () => load();
+'use strict';
 
 function load() {
     textArea.innerHTML = localStorage.getItem('brainbox');
@@ -9,6 +7,20 @@ function load() {
 function save() {
     const text = textArea.value;
     localStorage.setItem('brainbox', text);
+}
+
+const textArea = document.getElementById('note-text');
+textArea.addEventListener('keyup', save);
+
+window.onload = () => load();
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('service-worker.js')
+            .then((reg) => {
+                console.log('Service worker registered.', reg);
+            });
+    });
 }
 
 
