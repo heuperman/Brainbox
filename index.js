734bd1633325c9625cde41163366572ce5856e41
Cornelis Heuperman <cornelis.heuperman@rezonence.com>
2019-05-09 12:04:12 +0100

    Allow user to add notes

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
 'use strict';
 
 function load() {
-    textArea.innerHTML = localStorage.getItem('brainbox');
+    const data = JSON.parse(localStorage.getItem('brainbox'));
+    data ? createLoadedNotes(data) : addNote();
+}
+
+function createLoadedNotes(data) {
+    for (const entry in data) {
+        if (data.hasOwnProperty(entry)) {
+            const noteTextArea  = addNote();
+            noteTextArea.value = data[entry];
+        }
+    }
 }
 
 function save() {
-    const text = textArea.value;
-    localStorage.setItem('brainbox', text);
+    const textAreas = document.getElementsByClassName('note-text-area');
+    const data = {};
+    let index = 0;
+    for (const textArea of textAreas) {
+        data[index] = textArea.value;
+        index++;
+    }
+    console.log('Saving...', data);
+    localStorage.setItem('brainbox', JSON.stringify(data));
 }
 
-const textArea = document.getElementById('note-text');
-textArea.addEventListener('keyup', save);
+function addNote() {
+    const noteContainer = document.createElement('div');
+    noteContainer.classList.add('note-container');
+    const noteTextArea = document.createElement('textArea');
+    noteTextArea.classList.add('note-text-area');
+    const addNoteButton = document.getElementById('add-note-button');
+    noteContainer.appendChild(noteTextArea);
+    document.body.insertBefore(noteContainer, addNoteButton);
+    return noteTextArea;
+}
 
-window.onload = () => load();
+function addListeners() {
+    const textAreas = document.getElementsByClassName('note-text-area');
+    const addNoteButton = document.getElementById('add-note-button');
+    addNoteButton.addEventListener('click', addNote);
+    for (const textArea of textAreas) {
+        textArea.addEventListener('keyup', save);
+    }
+}
+
+window.onload = () => {
+    load();
+    addListeners();
+};
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
